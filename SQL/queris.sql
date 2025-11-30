63fc1f4fa9fafff78411f78718d320ef665f8927
Arman Arshak-Shahbazyan <arman.arshak.shahbazyan@gmail.com>
2025-11-30 21:08:58 +0400

    Customer payments, orders, and remaining debt

--- SQL/queris.sql
+++ SQL/queris.sql
@@ -6,3 +6,16 @@ LEFT JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber
 GROUP BY customers.customerName,customers.addressLine1
 ORDER BY revenue DESC
 LIMIT 5 ;
+
+
+
+SELECT customers.customerName,SUM(payments.amount) AS Done,
+sum(orderdetails.priceEach*orderdetails.quantityOrdered) AS
+Must,SUM(orderdetails.priceEach*orderdetails.quantityOrdered)-SUM(payments.amount) as Dubt
+FROM customers
+JOIN payments ON customers.customerNumber=payments.customerNumber
+JOIN orders on customers.customerNumber=orders.customerNumber
+JOIN orderdetails ON orders.orderNumber=orderdetails.orderNumber
+GROUP BY customers.customerName
+ORDER BY customers.customerName;
+
