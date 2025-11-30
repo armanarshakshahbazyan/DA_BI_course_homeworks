13f082292bc1a6b5afed9cf385792db364185768
Arman Arshak-Shahbazyan <arman.arshak.shahbazyan@gmail.com>
2025-11-30 21:14:48 +0400

    Order details with revenue, profit, and sales rep

--- SQL/queris.sql
+++ SQL/queris.sql
@@ -76,3 +76,42 @@ left join profit on profit.productName = products.productName
 
 -- left join name_surname on name_surname.customerNumber = orders.customerNumber
 ;
+
+
+
+with order_info AS (
+select customerNumber,productCode,orderDate,orderdetails.orderNumber,status,priceEach,
+sum(priceEach*quantityOrdered) AS revenue
+from orderdetails
+left join orders on orderdetails.orderNumber=orders.orderNumber
+Group by customerNumber,productCode, orderdetails.orderNumber,priceEach,status
+),
+
+profit as (
+SELECT products.productCode,products.productName,SUM((orderdetails.priceEach -
+products.buyPrice) * orderdetails.quantityOrdered) AS totalProfit
+FROM orderdetails
+JOIN products ON orderdetails.productCode = products.productCode
+GROUP BY products.productCode, products.productName
+ORDER BY totalProfit DESC
+)
+-- name_surname as (
+
+-- select customers.customerNumber,employees.firstName + employees.lastName as anunazg
+
+-- from customers
+
+-- left join employees on employees.employeeNumber = customers.salesRepEmployeeNumber
+-- left join orders on orders.customerNumber = customers.customerNumber
+-- )
+
+select order_info.orderDate,status,products.productName,customerName,city,revenue ,
+profit.totalProfit,
+CONCAT(employees.firstName, &#39; &#39;, employees.lastName) AS SalesRepName
+from order_info
+left join products on products.productCode = order_info.productCode
+left join customers on customers.customerNumber = order_info.customerNumber
+left join profit on profit.productName = products.productName
+LEFT JOIN employees ON employees.employeeNumber = customers.salesRepEmployeeNumber;
+-- left join name_surname on name_surname.customerNumber = orders.customerNumber
+;
