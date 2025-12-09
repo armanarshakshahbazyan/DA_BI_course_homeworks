4d91e3a9d982f49e60f626674ff2b4c1b1df1410
Arman Arshak-Shahbazyan <arman.arshak.shahbazyan@gmail.com>
2025-12-09 18:01:16 +0400

    moves

---
+++ PYTHON/HW 1.py
@@ -0,0 +1,12 @@
+# length = int(input("Enter length: "))
+# width = int(input("Enter width: "))
+# area = length * width
+
+# # print(area)
+
+# a=length = int(input())
+# b=width = int(input())
+
+# print(a*b)
+
+print("Hello world")
